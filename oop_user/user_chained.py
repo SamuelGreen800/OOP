6e8ce0f53b5b32d9769c89ffc1fe92d4b4c7b68e
Samuel Green <samuelgreen800@tutanota.com>
2022-01-10 13:02:35 -0500

    User2_chained

---
+++ oop_user/user_chained.py
@@ -0,0 +1,40 @@
+
+class User:
+    bank_name = "First National Dojo"
+    def __init__(self) -> None:
+        self.name = "Sam" 
+        self.email = "samgreen@dojo.com"
+        self.account_balance = 0
+        amount = 0
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self
+    
+    def withdrawl(self, amount):
+        self.account_balance -= amount
+        return self
+    def balance(self):
+        print(f"{self.name} --- {self.account_balance}")
+        return self
+    def transfer(self, amount, user):
+        self.account_balance -= amount
+        user.account_balance += amount
+        self.balance()
+        user.balance()
+        
+
+Sam = User()
+Sam.name = "Sam"
+Butters = User()
+Butters.name = "Butters"
+Monty = User()
+Monty.name = "Monty"
+
+Sam.deposit(100).deposit(40).deposit(20).withdrawl(80).balance()
+
+Butters.deposit(200).deposit(100).withdrawl(50).withdrawl(50).balance()
+
+Monty.deposit(300).withdrawl(100).withdrawl(100).withdrawl(20).balance()
+
+Monty.transfer(50, Sam)
